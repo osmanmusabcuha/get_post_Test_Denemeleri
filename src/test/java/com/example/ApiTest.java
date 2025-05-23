b0b0d51fd1fa7ca32b3816eefafa9b38369084b2
osman musap cuha <musabcuha@gmail.com>
2025-05-23 14:34:46 +0300

    proje yüklemesi

---
+++ src/test/java/com/example/ApiTest.java
@@ -0,0 +1,71 @@
+package com.example;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class ApiTest {
+
+    @Test
+    public void getAllPhonesWithAuth() {
+
+        String token = "Tokenı girmeniz gerek";
+
+
+        RestAssured.baseURI = "http://localhost:5000"; // kendi IP:Port’unu yaz
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .accept(ContentType.JSON)
+                .log().all()
+                .when()
+                .get("/api/phones")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("success", equalTo(true))
+                .body("phones[0].brand", equalTo("Apple"))
+                .body("phones[1].model", equalTo("Galaxy S21"))
+                .time(lessThan(1000L));
+    }
+
+
+
+    @Test
+    public void postPhone_ShouldReturnCreatedStatusAndValidResponse() {
+
+        RestAssured.baseURI = "http://localhost:5000";
+        String token = "Tokenı girmeniz gerek";
+
+        String uniqueIMEI = "99999999999" + (System.currentTimeMillis() % 10000);
+
+
+        String jsonBody = "{\n" +
+                "  \"brand\": \"Xiaomi\",\n" +
+                "  \"model\": \"Redmi Note 12\",\n" +
+                "  \"imei\": \"" + uniqueIMEI + "\",\n" +
+                "  \"price\": 13500,\n" +
+                "  \"warranty\": \"1 yıl\"\n" +
+                "}";
+
+
+        given()
+                .header("Authorization", token)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(jsonBody)
+                .when()
+                .post("/api/phones")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("success", equalTo(true))
+                .body("message", containsString("başarıyla"))
+                .body("phoneId", notNullValue())
+                .time(lessThan(1000L));
+    }
+
+}
